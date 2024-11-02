ef553b529ee88ef882d8d9945434ffa8c01a23d2
josedesgtt <josedesgtt@gmail.com>
2024-11-01 21:20:34 -0300

    actualizamos main.c con casos de etst

--- main.c
+++ main.c
@@ -19,181 +19,107 @@ void findAndPrintAll(struct keysPredict* kt, char* word ) {
     printf("Find: %s = %i\n", word, result==0);
 }
 
+// TESTING DEL CÓDIGO
 int main() {
 
-    // COMPLETAR
+    // EJERCICIO 1
+    // Preraramos las variables para testear
+    char* len1 = "aro";
+    char* len2 = "abcdefghijklmn-opqrstuvwxyz0123456789";
+    char* len3 = "";
+    char* len4 = "o";
 
-    // A Continuacion dejamos algunos ejemplos de como
-    // llamar a las funciones pedidas. Este codigo debe
-    // ser borrado / modificado.
+    // int strLen(char *src)
+    printf("Probando strLen(char *src);\n");
+    printf("strLen(\"%s%s%i%s", len1, "\") -> ", strLen(len1), "\n");
+    printf("strLen(\"%s%s%i%s", len2, "\") -> ", strLen(len2), "\n");
+    printf("strLen(\"%s%s%i%s", len3, "\") -> ", strLen(len3), "\n");
+    printf("strLen(\"%s%s%i%s", len4, "\") -> ", strLen(len4), "\n");
 
-
-    // strLen
-    int len = strLen("aro");
-    printf("strLen(\"aro\") -> \"%i\"\n", len);
+    // char* strDup(char *src)
+    char* dup1 = strDup(len1);
+    char* dup2 = strDup(len2);
+    char* dup3 = strDup(len3);
+    char* dup4 = strDup(len4);
+    printf("\nProbando strDup(char *src);\n");
+    printf("strDup(\"%s%s%s%s", len1, "\") -> \"", dup1, "\"\n");
+    printf("strDup(\"%s%s%s%s", len2, "\") -> \"", dup2, "\"\n");
+    printf("strDup(\"%s%s%s%s", len3, "\") -> \"", dup3, "\"\n");
+    printf("strDup(\"%s%s%s%s", len4, "\") -> \"", dup4, "\"\n");
+    free(dup1);
+    free(dup2);
+    free(dup3);
+    free(dup4);
     printf("\n");
 
-    // strDup
-    char* dup = strDup("aro");
-    printf("strDup(\"aro\") -> \"%s\"\n", dup);
-    free(dup);
-    printf("\n");
+    // EJERCICIO 2
+    // Preparamos las variables para testear
+    struct node *n1 = (struct node*)malloc(sizeof(struct node)), *n2 = (struct node*)malloc(sizeof(struct node)), *n3 = (struct node*)malloc(sizeof(struct node));
+    initNode(n1,'a',0,n2,0,0);
+    initNode(n2,'r',0,n3,0,0);
+    initNode(n3,'o',0,0,0,strDup(len1));
 
-    // Nuestro testing para funciones del ejercicio 2
-    /*
-    // struct node* findNodeInLevel(struct node** list, char character)
-    struct keysPredict* kt = (struct keysPredict*)malloc(sizeof(struct keysPredict));
 
-    struct node* n1 = (struct node*)malloc(sizeof(struct node));
-    struct node* n2 = (struct node*)malloc(sizeof(struct node));
-    struct node* n3 = (struct node*)malloc(sizeof(struct node));
-    n1->character = 'a';
-    n1->end = 0;
-    n1->next = 0;
-    n1->word = 0;
-    n1->down  = n2;
-    n2->character = 'r';
-    n2->end = 0;
-    n2->next = 0;
-    n2->word = 0;
-    n2->down = n3;
-    n2->character = 'o';
-    n3->end = 1;
-    n3->next = 0;
-    n3->word = dup;
-    n3->down = 0;
+    struct node *n11 = (struct node*)malloc(sizeof(struct node)), *n12 = (struct node*)malloc(sizeof(struct node)), *n13 = (struct node*)malloc(sizeof(struct node)), *n21 = (struct node*)malloc(sizeof(struct node)), *n22 = (struct node*)malloc(sizeof(struct node)), *n23 = (struct node*)malloc(sizeof(struct node)), *n31 = (struct node*)malloc(sizeof(struct node)), *n32 = (struct node*)malloc(sizeof(struct node)), *n33 = (struct node*)malloc(sizeof(struct node));
+    initNode(n11,'a',n12,n21,0,0);
+    initNode(n12,'b',n13,0,0,0);
+    initNode(n13,'d',0,0,0,0);
+    initNode(n21,'b',n22,n31,0,0);
+    initNode(n22,'c',n23,0,0,0);
+    initNode(n23,'d',0,0,0,0);
+    initNode(n31,'a',n32,0,0,0);
+    initNode(n32,'b',n33,0,0,0);
+    initNode(n33,'c',0,0,0,0);
 
-    char character= 'a';
-
-    struct node* nt = (struct node*)malloc(sizeof(struct node));
-    nt = findNodeInLevel(&n1, character);
-    //printf("Encontramos letra 'a' (1), no encontramos (0) - Resultado: %p\n", (void*)nt);
-    printf("Encontramos letra 'a', no encontramos (0) - Resultado: %c\n", nt->character);
-
-    struct node* nn1 = (struct node*)malloc(sizeof(struct node));
-    struct node* nn2 = (struct node*)malloc(sizeof(struct node));
-    struct node* nn3 = (struct node*)malloc(sizeof(struct node));
-
-    struct node* nnt = (struct node*)malloc(sizeof(struct node));
-    nnt = findNodeInLevel(&nn1, character);
-    printf("Encontramos letra 'a', no encontramos (0) - Resultado: %p\n", (void*)nnt);
+    // struct node *findNodeInLevel(struct node **list, char character)
+    printf("Buscamos letras con findNodeInLevel (Si=1/No=0) \n-Primer nivel:\n");
+    printf("Encontramos \'a\'?: %i%s", !(findNodeInLevel(&n1, 'a') == 0), "\n");
+    printf("Encontramos \'y\'?: %i%s", !(findNodeInLevel(&n1, 'y') == 0), "\n");
+    printf("Encontramos \'8\'?: %i%s", !(findNodeInLevel(&n1, '8') == 0), "\n");
+    printf("-Segundo nivel:\n");
+    printf("Encontramos \'r\'?: %i%s", !(findNodeInLevel(&n2, 'r') == 0), "\n");
+    printf("Encontramos \'t\'?: %i%s", !(findNodeInLevel(&n2, 't') == 0), "\n");
+    printf("Encontramos \'7\'?: %i%s", !(findNodeInLevel(&n2, '7') == 0), "\n");
+    printf("-Tercer nivel:\n");
+    printf("Encontramos \'o\'?: %i%s", !(findNodeInLevel(&n3, 'o') == 0), "\n");
+    printf("Encontramos \'w\'?: %i%s", !(findNodeInLevel(&n3, 'w') == 0), "\n");
+    printf("Encontramos \'4\'?: %i%s", !(findNodeInLevel(&n3, '4') == 0), "\n");
 
     // struct node* addSortedNewNodeInLevel(struct node** list, char character)
+    struct keysPredict* kt_aux = keysPredictNew();
+    kt_aux->first = n11;
+printf("\nAgregamos letras con addSortedNewNodeInLevel\nAntes de añadir:\n");
+    keysPredictPrint(kt_aux);
+    printf("\nAñadimos \'c\' en el primer nivel, \'a\' en el segundo y \' \':\n");
+    kt_aux->first = addSortedNewNodeInLevel(&(kt_aux->first), 'c');
+    kt_aux->first->down = addSortedNewNodeInLevel(&kt_aux->first->down, 'a');
+    kt_aux->first->down->down = addSortedNewNodeInLevel(&kt_aux->first->down->down, ' ');
+    keysPredictPrint(kt_aux);
+    free(kt_aux);
 
-    struct node* n11 = (struct node*)malloc(sizeof(struct node));
-    struct node* n12 = (struct node*)malloc(sizeof(struct node));
-    struct node* n13 = (struct node*)malloc(sizeof(struct node));
+    // EJERCICIO 3
 
-    struct node* n21 = (struct node*)malloc(sizeof(struct node));
-    struct node* n22 = (struct node*)malloc(sizeof(struct node));
-    struct node* n23 = (struct node*)malloc(sizeof(struct node));
-
-    struct node* n31 = (struct node*)malloc(sizeof(struct node));
-    struct node* n32 = (struct node*)malloc(sizeof(struct node));
-    struct node* n33 = (struct node*)malloc(sizeof(struct node));
-
-
-    n11->character = 'a';
-    n11->end = 0;
-    n11->next = n12;
-    n11->word = 0;
-    n11->down  = n21;
-
-    n12->character =  'b';
-    n12->end = 0;
-    n12->next = n13;
-    n12->word = 0;
-    n12->down = 0;
-
-    n13->character = 'd';
-    n13->end = 0;
-    n13->next = 0;
-    n13->word = 0;
-    n13->down = 0;
-
-    n21->character = 'b';
-    n21->end = 0;
-    n21->next = n22;
-    n21->word = 0;
-    n21->down  = n31;
-
-    n22->character =  'c';
-    n22->end = 0;
-    n22->next = n23;
-    n22->word = 0;
-    n22->down = 0;
-
-    n23->character = 'd';
-    n13->end = 0;
-    n13->next = 0;
-    n13->word = 0;
-    n13->down = 0;
-
-    n31->character = 'a';
-    n31->end = 0;
-    n31->next = n32;
-    n31->word = 0;
-    n31->down  = 0;
-
-    n32->character = 'b';
-    n32->end = 0;
-    n32->next = n33;
-    n32->word = 0;
-    n32->down  = 0;
-
-    n33->character = 'c';
-    n33->end = 0;
-    n33->next = 0;
-    n33->word = 0;
-    n33->down  = 0;
-
-    kt->first = n11;
-    kt->totalKeys = 3;
-    kt->totalWords = 0;
-
-    printf("El caracter siguiente deberÃ­a ser b:  %c\n", addSortedNewNodeInLevel(&n11->down, 'a')->next->character);
-    keysPredictPrint(kt);
-
-
-    */
-
-    // Testing oficial ejercicio 3
-
-    // keysPredict
     struct keysPredict* kt = keysPredictNew();
 
-    // keysPredict - crear un diccionario
-    keysPredictAddWord(kt, "papanatas");
-
-    keysPredictAddWord(kt, "zanahoria");
-    keysPredictAddWord(kt, "oreja");
-    keysPredictAddWord(kt, "ricardo");
-    keysPredictAddWord(kt, "zaz");
-    keysPredictAddWord(kt, "a");
-    keysPredictAddWord(kt, "");
-    keysPredictAddWord(kt, "b");
-    keysPredictAddWord(kt, "ab");
-    keysPredictAddWord(kt, "abfff");
-    keysPredictAddWord(kt, "abfgato");
-    keysPredictAddWord(kt, "bebe");
-    keysPredictAddWord(kt, "pata");
+    // Testeamos con casos chicos
+    printf("\nTesteamos casos con keysPredict chicos:\n");
+    keysPredictAddWord(kt, "alfajor");
+    keysPredictAddWord(kt, "canoa");
+    keysPredictAddWord(kt, "rinoceronte");
+    keysPredictAddWord(kt, "casa");
+    keysPredictAddWord(kt, "rino");
     keysPredictPrint(kt);
+    keysPredictRemoveWord(kt, "canoa");
+    keysPredictAddWord(kt, "pato");
+    keysPredictPrint(kt);
+    predictAndPrintAll(kt,"c");
+    predictAndPrintAll(kt,"ca");
+    predictAndPrintAll(kt,"casa");
+    predictAndPrintAll(kt,"casas");
 
-    // keysPredict - listar todas las palabras
-    char** words, **words2;
-    int wordsCount;
-    words = keysPredictListAll(kt, &wordsCount);
-    printf("Palabras encontradas: \n");
-    for(int i=0; i<wordsCount; i++) {
-        printf("\"%s\"\n", words[i]);
-    }
-    printf("Palabras con prefijo \"za\": \n");
-    words2 = keysPredictRun(kt, "za", &wordsCount);
-    for(int i=0; i<2; i++) {
-        printf("\"%s\"\n", words2[i]);
-    }
-    printf("%i", keysPredictCountWordAux(kt->first));
-    deleteArrayOfWords(words, wordsCount);
+    // Testeamos con casos grandes
+
+
 
     // keysPredict - encontrar palabras
 
@@ -210,6 +136,7 @@ int main() {
     predictAndPrintAll(kt,"pap");
     predictAndPrintAll(kt,"q");
     predictAndPrintAll(kt,"zap");
+    predictAndPrintAll(kt, "a");
 
     // keysPredict - predecir palabras
 
@@ -218,6 +145,7 @@ int main() {
     keysPredictRemoveWord(kt,"aaa");
     keysPredictRemoveWord(kt,"papa");
     keysPredictRemoveWord(kt,"pata");
+    keysPredictRemoveWord(kt,"q");
     keysPredictPrint(kt);
 
     // keysPredict - borrar diccionario

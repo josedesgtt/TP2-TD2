28952fb21234b5b67dfaabf3fa50ec570f7ddffc
Arotu <arotuoficial@gmail.com>
2024-11-02 23:54:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void predictAndPrintAll(struct keysPredict* kt, char* partialWord ) {
     words = keysPredictRun(kt, partialWord, &wordsCount);
     printf("Predicts (%i): %s = ", wordsCount, partialWord);
     for(int i=0; i<wordsCount; i++) {
-        printf("%s", words[i]);
+        printf("%s, ", words[i]);
     }
     printf("\n");
     deleteArrayOfWords(words, wordsCount);
@@ -36,7 +36,7 @@ int main() {
     // Preraramos las variables para testear
     char* len1 = "aro";
     char* len2 = "abcdefghijklmn-opqrstuvwxyz0123456789";
-    char* len3 = "";
+    char* len3 = "dicho";
     char* len4 = "o";
 
     // int strLen(char *src)
@@ -56,19 +56,18 @@ int main() {
     printf("strDup(\"%s%s%s%s", len2, "\") -> \"", dup2, "\"\n");
     printf("strDup(\"%s%s%s%s", len3, "\") -> \"", dup3, "\"\n");
     printf("strDup(\"%s%s%s%s", len4, "\") -> \"", dup4, "\"\n");
-    free(dup1);
-    free(dup2);
-    free(dup3);
-    free(dup4);
     printf("\n");
 
     // EJERCICIO 2
     // Preparamos las variables para testear
-    struct node *n1 = (struct node*)malloc(sizeof(struct node)), *n2 = (struct node*)malloc(sizeof(struct node)), *n3 = (struct node*)malloc(sizeof(struct node));
+    struct node *n1 = (struct node*)malloc(sizeof(struct node)),*n2 = (struct node*)malloc(sizeof(struct node)), *n3 = (struct node*)malloc(sizeof(struct node));
     initNode(n1,'a',0,n2,0,0);
     initNode(n2,'r',0,n3,0,0);
-    initNode(n3,'o',0,0,0,strDup(len1));
-
+    initNode(n3,'o',0,0,0,dup1);
+    free(dup1);
+    free(dup2);
+    free(dup3);
+    free(dup4);
 
     struct node *n11 = (struct node*)malloc(sizeof(struct node)), *n12 = (struct node*)malloc(sizeof(struct node)), *n13 = (struct node*)malloc(sizeof(struct node)), *n21 = (struct node*)malloc(sizeof(struct node)), *n22 = (struct node*)malloc(sizeof(struct node)), *n23 = (struct node*)malloc(sizeof(struct node)), *n31 = (struct node*)malloc(sizeof(struct node)), *n32 = (struct node*)malloc(sizeof(struct node)), *n33 = (struct node*)malloc(sizeof(struct node));
     initNode(n11,'a',n12,n21,0,0);
@@ -95,6 +94,7 @@ int main() {
     printf("Encontramos \'w\'?: %i%s", !(findNodeInLevel(&n3, 'w') == 0), "\n");
     printf("Encontramos \'4\'?: %i%s", !(findNodeInLevel(&n3, '4') == 0), "\n");
 
+
     // struct node* addSortedNewNodeInLevel(struct node** list, char character)
     struct keysPredict* kt_aux = keysPredictNew();
     kt_aux->first = n11;
@@ -105,6 +105,10 @@ int main() {
     kt_aux->first->down = addSortedNewNodeInLevel(&kt_aux->first->down, 'a');
     kt_aux->first->down->down = addSortedNewNodeInLevel(&kt_aux->first->down->down, ' ');
     keysPredictPrint(kt_aux);
+    keysPredictDelete(kt_aux);
+    free(n1);
+    free(n2);
+    free(n3);
 
     // EJERCICIO 3
 
@@ -119,6 +123,7 @@ int main() {
     keysPredictAddWord(kt, "rino");
     keysPredictAddWord(kt, "camion");
     keysPredictPrint(kt);
+
     predictAndPrintAll(kt,"c");
     predictAndPrintAll(kt,"ca");
     predictAndPrintAll(kt, "cam");
@@ -128,7 +133,9 @@ int main() {
     predictAndPrintAll(kt, "c");
     predictAndPrintAll(kt,"casas");
 
-    // Testeamos con casos grandes
+
+    // Testeamos con casos grandes: Usamos el predict.c y dicc.txt, probamos todas las combinaciones de prefijos de dos letras posibles.
+    // También probamos borrando la mitad y más del diccionario.
 
     // keysPredict - encontrar palabras
 
@@ -161,3 +168,4 @@ int main() {
     keysPredictDelete(kt);
     return 0;
 }
+
